deb61613c937277370188d0919a19972b10b5172
Jin <jbjxu@thoughtworks.com>
2019-02-22 15:53:36 +0800

    feat: can log out by /api/logout api

--- src/main/java/com/summary/spring/authentication/controller/AuthController.java
+++ src/main/java/com/summary/spring/authentication/controller/AuthController.java
@@ -4,12 +4,19 @@ import com.summary.spring.authentication.Entity.User;
 import com.summary.spring.authentication.Model.RegisterDTO;
 import com.summary.spring.authentication.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -19,6 +26,12 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private TokenStore tokenStore;
+
+    @Autowired
+    private ConsumerTokenServices tokenService;
 
     @PostMapping(value = "/register")
     public User registerNewUser(@RequestBody RegisterDTO user) {
@@ -48,11 +61,24 @@ public class AuthController {
             return currentUserName;
         }
         return authentication.getName();
-
     }
 
     @GetMapping(value = "/users/{userName}")
     public User getUserByName(@PathVariable(value = "userName") String userName) {
         return userRepository.findByUserName(userName);
+    }
+
+    @GetMapping(value = "/logout")
+    public ResponseEntity logout(@RequestHeader(value = "Authorization", required = false) String authHeader) {
+        if (Objects.nonNull(authHeader) && authHeader.startsWith("Bearer")) {
+            String tokenValue = authHeader.replace("Bearer", "").trim();
+            OAuth2AccessToken accessToken = tokenStore.readAccessToken(tokenValue);
+
+            tokenStore.removeAccessToken(accessToken);
+            tokenService.revokeToken(String.valueOf(accessToken));
+            
+            return new ResponseEntity("logout successful!", HttpStatus.OK);
+        }
+        return new ResponseEntity("logout failed!", HttpStatus.BAD_REQUEST);
     }
 }
